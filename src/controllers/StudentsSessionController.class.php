374075dffa6eaa7fcaf50800c2e7ead41590bcb5
JrBour <calaruni@hotmail.com>
2019-03-27 10:03:12 +0100

    feat(studentsSession): Edit the student note

--- src/controllers/StudentsSessionController.class.php
+++ src/controllers/StudentsSessionController.class.php
@@ -58,38 +58,21 @@ class StudentsSessionController
     {
         $post = $params['POST'];
         $id = $params['URL'][0];
-        $session = Manager::$em->find(Session::class, $id);
-        $formations = Manager::$em->getRepository(Formation::class)->findAll();
-        $rooms = Manager::$em->getRepository(Room::class)->findAll();
-        $teachers = Manager::$em->getRepository(Person::class)->findBy(['role' => 'ROLE_TEACHER']);
+        $studentsSession = Manager::$em->find(StudentsSession::class, $id);
 
         if (count($post) > 0) {
-			$formation = Manager::$em->find(Formation::class, $post['formation']);
-			$teacher = Manager::$em->find(Person::class, $post['teacher']);
-			$room = Manager::$em->find(Room::class, $post['room']);
+			$studentsSession->setNote($post['note']);
 			
-			$session->setStarting(new \Datetime($post['date'] . $post['startHour']));
-			$session->setEnding(new \Datetime($post['date'] . $post['endHour']));
-			$session->setReport($post['report']);
-			$session->setFormations($formation);
-			$session->setTeacher($teacher);
-			$session->setRooms($room);
-			
-			Manager::$em->persist($session);
+			Manager::$em->persist($studentsSession);
 			Manager::$em->flush();
 			
-			$sessions = Manager::$em->getRepository(Session::class)->findAll();
-			
-			return Views::render('session.index', [
-				'sessions' => $sessions
+			return Views::render('studentsSession.show', [
+				'studentsSession' => $studentsSession
 			]);
         }
 
-        return Views::render('session.edit', [
-            'session' => $session,
-            'teachers' => $teachers,
-            'formations' => $formations,
-            'rooms' => $rooms
+        return Views::render('studentsSession.edit', [
+            'studentsSession' => $studentsSession
         ]);
 	}
 
